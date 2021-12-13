c9f73b9d1dea1aef9efbbaa29a7f4b17c271c43f
Dan <dan@waxworlds.org>
2021-12-13 13:32:45 +0000

    DB Setup Wizard Working

--- server/configuredb.sh
+++ server/configuredb.sh
@@ -6,7 +6,26 @@
 #####################
 
 echo
-echo "Welcome to the secure login DB setup wizzard"
+echo "##################################################"
+echo "#                                                #"
+echo "#  Welcome to the secure login DB setup wizard   #"
+echo "#                                                #"
+echo "##################################################"
+echo
+echo "This batch file will gather the necessary details from you to set up the"
+echo "secure login database."
+echo "I appreciate your concerns, should you have them, about running unknown"
+echo "shell scripts acquired from the internet and I would strongly recommend"
+echo "that you have a good look though it before you run it."
+echo "That being said, I assure you there is nothing nefarious in this script,"
+echo "it will create a file called dbconfig.json with the info you supply and"
+echo "then give you the option of creating the database you name and/or"
+echo "creating the secure login users table."
+echo "If your database is being used by multiple applications I would recommend"
+echo "adding a prefix to the table name."
+echo
+echo "Lets begin..."
+echo
 if [ -e "./dbconfig.JSON" ]
 then
   echo
@@ -15,23 +34,19 @@ then
   exit 1
 else
 echo "Please enter the user name for your MySql server: "
-  echo
   read user
   echo
   echo "Please enter the password: "
-  echo
   read password
   echo
   echo "Please enter the name of the database: "
-  echo
   read database
   echo
   echo "please enter any prefix you may want (for databases used by muliple applications)"
-  echo
   read prefix
   echo
   echo
-  echo "Thank you, generating dbconfig.js now..."
+  echo "Thank you, generating dbconfig.json now..."
   touch ./dbconfig.json
   echo "{
   \"host\": \"localhost\",
@@ -41,42 +56,67 @@ echo "Please enter the user name for your MySql server: "
   \"prefix\": \"$prefix\"
 }" > ./dbconfig.JSON
 
-  echo
   echo "dbconfig.json successfully created."
+  echo
   echo "would you like to create the database ($database) now (y/n)?"
   read answer
-  if [ answer = 'y' ]
+  if [ $answer == 'y' ]
   then
-    mysql -u$user -p$password -e "CREATE DATABASE IF NOT EXISTS $database"
     echo
-    if [$? > 0]
+    echo "Running the following query now..."
+    echo "CREATE DATABASE IF NOT EXISTS ${database};"
+    mysql -u$user -p$password -e "CREATE DATABASE IF NOT EXISTS ${database};"
+    echo
+    if [ $? -gt 0 ]
     then
-      echo "something went wrong, perhaps the database already exists!"
+      echo "Appologies, something went wrong! Please contact your IT technician"
+      exit 1
     fi
   fi
 
   echo
   echo "Would you like to create the table ${prefix}users now (y/n)? "
   read answer
-  if [ answer = 'y' ]
+  if [ $answer == 'y' ]
   then
-    mysql -u$user -p$password -e "USE ${database}; CREATE TABLE `${prefix}users` (
-`id` int NOT NULL AUTO_INCREMENT,
-`datecreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-`datelastlogin` datetime DEFAULT NULL,
-`username` varchar(45) NOT NULL,
-`email` varchar(256) NOT NULL,
-`password` varchar(256) NOT NULL,
-`phone` varchar(15) DEFAULT NULL,
-`address` varchar(256) DEFAULT NULL,
-`dob` date DEFAULT NULL,
-`gender` varchar(20) DEFAULT NULL,
-PRIMARY KEY (`id`),
-UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
-    if [$? > 0]
+    echo "Running the following query now..."
+    echo "CREATE TABLE ${prefix}users (id int NOT NULL AUTO_INCREMENT,
+datecreated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+datelastlogin datetime DEFAULT NULL,
+username varchar(45) NOT NULL,
+email varchar(256) NOT NULL,
+password varchar(256) NOT NULL,
+phone varchar(15) DEFAULT NULL,
+address varchar(256) DEFAULT NULL,
+dob date DEFAULT NULL,
+gender varchar(20) DEFAULT NULL,
+PRIMARY KEY (id),
+UNIQUE KEY id_UNIQUE (id) )
+ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;"
+
+    mysql -u$user -D $database -p$password <<EOF
+
+CREATE TABLE ${prefix}users (id int NOT NULL AUTO_INCREMENT,
+datecreated datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+datelastlogin datetime DEFAULT NULL,
+username varchar(45) NOT NULL,
+email varchar(256) NOT NULL,
+password varchar(256) NOT NULL,
+phone varchar(15) DEFAULT NULL,
+address varchar(256) DEFAULT NULL,
+dob date DEFAULT NULL,
+gender varchar(20) DEFAULT NULL,
+PRIMARY KEY (id),
+UNIQUE KEY id_UNIQUE (id) )
+ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+EOF
+
+    if [ $? -gt 0 ]
     then
       echo "something went wrong while trying to create the table ${database}.${prefix}users!"
+      echo "Please contact your IT technician"
+      exit 1
     fi
   fi
   echo
